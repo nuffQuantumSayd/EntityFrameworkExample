d10c694eab4da14590e420fe8abf2aa5db906176
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-07-18 11:23:30 -0700

    Added Microsoft documentation link

--- EntityFrameworkExample/StudentContext.cs
+++ EntityFrameworkExample/StudentContext.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 
 namespace EntityFrameworkExample
 {
+	// EF Core Getting started
+	// https://learn.microsoft.com/en-us/ef/core/get-started/overview/first-app?source=recommendations&tabs=netcore-cli
 	internal class StudentContext : DbContext 
 	{
 
