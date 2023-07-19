686da57ddb29c44c07f33c7c287a422e907b1633
nuffQuantumSayd <aaronkornish.ak@gmail.com>
2023-07-19 11:20:36 -0700

    Created a Student model to track in the database

---
+++ EntityFrameworkExample/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkExample
+{
+    public class Student
+    {
+        [Key] // Tell EF Core to make this a Primary Key (PK)
+        public int Id { get; set; }
+
+        public string FullName { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}
